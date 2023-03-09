8800bbccc4da2b1b51a096472f8662e2461a2df3
yogi <yogi@gmail.cOm>
2023-03-09 23:06:00 +0530

    add code

---
+++ main.tf
@@ -0,0 +1,20 @@
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+module "vpc" {
+  source          = "./vpc"
+  vpc_cidr        = "10.0.0.0/16"
+  public_cidrs    = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_cidrs   = ["10.0.3.0/24", "10.0.4.0/24"]
+  transit_gateway = "${module.transit_gateway.transit_gateway}"
+}
+
+# module "ec2" {
+#   source         = "./ec2"
+#   my_public_key  = "/tmp/id_rsa.pub"
+#   instance_type  = "t2.micro"
+#   security_group = "${module.vpc.security_group}"
+#   subnets        = "${module.vpc.public_subnets}"
+# }
